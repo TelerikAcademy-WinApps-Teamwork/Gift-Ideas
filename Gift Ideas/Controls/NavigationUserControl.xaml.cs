4e3ef66c1312f8b6b18980b72e487b4754a7157b
Nikolai Novkirishki <novkirishki94@gmail.com>
2015-12-18 22:41:18 +0200

    Created user control navigation

---
+++ Gift Ideas/Controls/NavigationUserControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Gift_Ideas.Controls
+{
+    using Pages;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class NavigationUserControl : UserControl
+    {
+        public NavigationUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnHomeButtonClick(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(ListGiftsPage), null);
+        }
+    }
+}
