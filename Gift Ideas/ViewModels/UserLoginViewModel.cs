141ad19781705e7d83fbd396fd49728cab603f57
Nikolai Novkirishki <novkirishki94@gmail.com>
2015-12-18 11:23:38 +0200

    Added login/register UI and view models

---
+++ Gift Ideas/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Gift_Ideas.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public UserLoginViewModel() 
+            :this(string.Empty, string.Empty)
+        {
+        }
+
+        public UserLoginViewModel(string email, string password)
+        {
+            this.Email = email;
+            this.Password = password;
+        }
+    }
+}
---
+++ Gift Ideas/ViewModels/UserRegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Gift_Ideas.ViewModels
+{
+    public class UserRegisterViewModel
+    {
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public UserRegisterViewModel()
+            : this(string.Empty, string.Empty, string.Empty)
+        {
+        }
+
+        public UserRegisterViewModel(string email, string password, string username)
+        {
+            this.Email = email;
+            this.Password = password;
+            this.Username = username;
+        }
+    }
+}
